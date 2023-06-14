cde2b5f29f2ca12e967508d5366c8ea71ed4baaf
Natasha Bartley <+>
2023-06-13 23:46:24 -0400

    slide show  rendering

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,14 +1,12 @@
 import React from "react";
 import Slideshow from "./Slideshow.jsx"
 import EventsPage from "./EventsPage";
-import FeaturedEvents from "./FeaturedEvents";
 import EventsMap from "./EventsMap.jsx";
 
 function Home(){
     return(
         <div>
             <Slideshow />
-            <FeaturedEvents />
             <EventsPage />
             <EventsMap />
         </div>
